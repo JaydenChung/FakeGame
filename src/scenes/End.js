81af07f30357da1d3d34468e93af693c53f87c96
alitaquie <123027814+alitaquie@users.noreply.github.com>
2024-03-18 14:11:12 -0700

    end scene

--- src/scenes/End.js
+++ src/scenes/End.js
@@ -6,16 +6,53 @@ class End extends Phaser.Scene{
     preload(){
     }
     create(){
+        this.cameras.main.setBackgroundColor('#f5624c');
+        //display game over
+        let gameOverConfig = {
+            fontFamily: 'fantasy',
+            fontSize: '36px',
+            backgroundColor: '#f5624c',
+            color: '#000000',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        
         this.EndMusic = this.sound.add("EndMusic")
         this.EndMusic.play()
 //         //displaygameover
 //         const gameOverImage = this.add.image(0, 0, 'gameOver').setOrigin(0,0);
 
-        this.add.text(this.game.config.width-250, 350, 'The dragon got away :(', {
+        this.add.text(this.game.config.width-400, 200, 'GAME OVER!', {
             fontSize: '62px',
             fill: '#fff',
             align: 'center'
           }).setOrigin(1, 0);
+
+        this.add.text(this.game.config.width-100, 300, 'The dragon managed to escape!', {
+            fontSize: '62px',
+            fill: '#fff',
+            align: 'center'
+          }).setOrigin(1, 0);
+        
+        this.add.text(this.game.config.width-255, 400, 'Press R to Restart', {
+            fontSize: '62px',
+            fill: '#fff',
+            align: 'center'
+          }).setOrigin(1, 0);
+
+        this.add.text(this.game.config.width-225, 500, 'Press C for credits', {
+            fontSize: '62px',
+            fill: '#fff',
+            align: 'center'
+          }).setOrigin(1, 0);
+        
+        
+
+          
     }
 }
 
@@ -49,3 +86,5 @@ class End extends Phaser.Scene{
 //         }
 //     }
 // }
+
+
